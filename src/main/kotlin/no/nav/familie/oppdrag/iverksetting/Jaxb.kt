d6b3fd1a3274094053d43d89be06b8fd5654057a
Martine Enger <marenger@student.matnat.uio.no>
2019-12-09 15:07:05 +0100

    Unmarshal melding på kvitteringskø og hent ut status og svarmelding

--- src/main/kotlin/no/nav/familie/oppdrag/iverksetting/Jaxb.kt
+++ src/main/kotlin/no/nav/familie/oppdrag/iverksetting/Jaxb.kt
@@ -1,14 +1,34 @@
 package no.nav.familie.oppdrag.iverksetting
 
 import no.trygdeetaten.skjema.oppdrag.Oppdrag
+import java.io.StringReader
 import java.io.StringWriter
 import javax.xml.bind.JAXBContext
 import javax.xml.bind.Marshaller
+import javax.xml.stream.XMLInputFactory
+import javax.xml.transform.stream.StreamSource
 
 class Jaxb {
 
-    val marshaller = JAXBContext.newInstance(Oppdrag::class.java).createMarshaller().apply {
+    val jaxbContext = JAXBContext.newInstance(Oppdrag::class.java)
+    val marshaller = jaxbContext.createMarshaller().apply {
         setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true)
+    }
+    val unmarshaller = jaxbContext.createUnmarshaller()
+
+    val xmlInputFactory = XMLInputFactory.newInstance().apply {
+        setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, false)
+        setProperty(XMLInputFactory.SUPPORT_DTD, false)
+    }
+
+
+    fun tilOppdrag(oppdragXml: String): Oppdrag {
+        val oppdrag = unmarshaller.unmarshal(
+                xmlInputFactory.createXMLStreamReader(StreamSource(StringReader(oppdragXml))),
+                Oppdrag::class.java
+        )
+
+        return oppdrag.value
     }
 
     fun tilXml(oppdrag: Oppdrag): String {
--- src/main/kotlin/no/nav/familie/oppdrag/iverksetting/OppdragMottaker.kt
+++ src/main/kotlin/no/nav/familie/oppdrag/iverksetting/OppdragMottaker.kt
@@ -1,5 +1,6 @@
 package no.nav.familie.oppdrag.iverksetting
 
+import no.trygdeetaten.skjema.oppdrag.Oppdrag
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.jms.annotation.JmsListener
@@ -14,7 +15,19 @@ class OppdragMottaker(@Autowired val jmsTemplateInngående: JmsTemplate) {
         LOG.info("Lytter på kvitteringskø: {}", jmsTemplateInngående.defaultDestinationName)
 
         val melding = jmsTemplateInngående.receiveAndConvert()
-        LOG.info("Mottatt melding på kvitteringskø: {}", melding)
+
+        val oppdragKvittering = Jaxb().tilOppdrag(melding as String)
+        val status = hentStatus(oppdragKvittering)
+        val svarMelding = hentMelding(oppdragKvittering)
+        LOG.info("Mottatt melding på kvitteringskø med status $status og svar $svarMelding")
+    }
+
+    private fun hentStatus(kvittering: Oppdrag): Status {
+        return Status.fromKode(kvittering.mmel.alvorlighetsgrad)
+    }
+
+    private fun hentMelding(kvittering: Oppdrag): String {
+        return kvittering.mmel.beskrMelding
     }
 
     companion object {
