a59ff9dc46b3676e39806093969347b3e26eb98c
Jørn Ola Birkeland <jorn.ola.birkeland@nav.no>
2019-12-12 11:48:11 +0100

    [kaizen] Legger til lagring av mottatt oppdragsmeldinger i en database, slik at kvitteringer kan matches mot innsendte meldinger

---
+++ src/main/kotlin/no/nav/familie/oppdrag/repository/OppdragProtokollStatus.kt
@@ -0,0 +1,5 @@
+package no.nav.familie.oppdrag.repository
+
+enum class OppdragProtokollStatus {
+    LAGT_PÅ_KØ, KVITTERT_OK, KVITTERT_MED_MANGLER, KVITTERT_FEIL
+}
--- src/main/kotlin/no/nav/familie/oppdrag/rest/OppdragController.kt
+++ src/main/kotlin/no/nav/familie/oppdrag/rest/OppdragController.kt
@@ -2,21 +2,30 @@ package no.nav.familie.oppdrag.rest
 
 import no.nav.familie.oppdrag.iverksetting.OppdragMapper
 import no.nav.familie.oppdrag.iverksetting.OppdragSender
+import no.nav.familie.oppdrag.repository.OppdragProtokoll
+import no.nav.familie.oppdrag.repository.OppdragProtokollRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+
 @RestController
 @RequestMapping("/api")
-class OppdragController(@Autowired val oppdragSender: OppdragSender, @Autowired val oppdragMapper: OppdragMapper) {
+class OppdragController(@Autowired val oppdragSender: OppdragSender,
+                        @Autowired val oppdragMapper: OppdragMapper,
+                        @Autowired val oppdragProtokollRepository: OppdragProtokollRepository) {
 
     @Deprecated("Dette endepunktet brukes kun for å teste integrasjonen mot OS over MQ")
     @GetMapping("/oppdrag")
     fun sendOppdrag(): String {
 
         val oppdrag110 = oppdragMapper.tilOppdrag110()
-        oppdragSender.sendOppdrag(oppdragMapper.tilOppdrag(oppdrag110))
+        val oppdrag = oppdragMapper.tilOppdrag(oppdrag110)
+
+        // TODO flytt disse to til en @Transactional + @Service type klasse
+        oppdragSender.sendOppdrag(oppdrag)
+        oppdragProtokollRepository.save(OppdragProtokoll.lagFraOppdrag(oppdrag))
         return "Oppdrag sendt ok"
     }
 }
