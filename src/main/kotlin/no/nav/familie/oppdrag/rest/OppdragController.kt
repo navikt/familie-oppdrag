b865c33241624cdb848fc0dd90ca532139655dc1
Jørn Ola Birkeland <jorn.ola.birkeland@nav.no>
2019-12-12 11:48:11 +0100

    [kaizen] Legger til lagring av mottatt oppdragsmeldinger i en database, slik at kvitteringer kan matches mot innsendte meldinger

--- src/main/kotlin/no/nav/familie/oppdrag/rest/OppdragController.kt
+++ src/main/kotlin/no/nav/familie/oppdrag/rest/OppdragController.kt
@@ -4,20 +4,27 @@ import no.nav.familie.kontrakter.felles.oppdrag.Utbetalingsoppdrag
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.familie.oppdrag.iverksetting.OppdragMapper
 import no.nav.familie.oppdrag.iverksetting.OppdragSender
+import no.nav.familie.oppdrag.repository.OppdragProtokoll
+import no.nav.familie.oppdrag.repository.OppdragProtokollRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
+
 @RestController
 @RequestMapping("/api")
-class OppdragController(@Autowired val oppdragSender: OppdragSender, @Autowired val oppdragMapper: OppdragMapper) {
+class OppdragController(@Autowired val oppdragSender: OppdragSender,
+                        @Autowired val oppdragMapper: OppdragMapper,
+                        @Autowired val oppdragProtokollRepository: OppdragProtokollRepository) {
 
    @PostMapping(consumes = [MediaType.APPLICATION_JSON_VALUE], path = ["/oppdrag"])
    fun sendOppdrag(@Valid @RequestBody utbetalingsoppdrag: Utbetalingsoppdrag): ResponseEntity<Ressurs<String>> {
         val oppdrag110 = oppdragMapper.tilOppdrag110(utbetalingsoppdrag)
-        oppdragSender.sendOppdrag(oppdragMapper.tilOppdrag(oppdrag110))
+       val oppdrag = oppdragMapper.tilOppdrag(oppdrag110)
+       oppdragSender.sendOppdrag(oppdrag)
+       oppdragProtokollRepository.save(OppdragProtokoll.lagFraOppdrag(oppdrag))
         return ResponseEntity.ok().body(Ressurs.Companion.success("Oppdrag sendt ok"))
     }
 }
