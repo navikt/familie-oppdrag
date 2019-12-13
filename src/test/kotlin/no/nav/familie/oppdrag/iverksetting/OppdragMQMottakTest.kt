1e576f61e4b8e47e81beb5ac15cc29f0e1649337
Martine Enger <marenger@student.matnat.uio.no>
2019-12-13 12:35:12 +0100

    Hent kvitterings-xml for å bruke i test

---
+++ src/test/kotlin/no/nav/familie/oppdrag/iverksetting/OppdragMQMottakTest.kt
@@ -0,0 +1,22 @@
+package no.nav.familie.oppdrag.iverksetting
+
+import org.junit.jupiter.api.Test
+
+class OppdragMQMottakTest {
+
+
+    @Test
+    fun skal_tolke_kvittering_riktig_ved_OK() {
+
+    }
+
+    @Test
+    fun skal_tolke_kvittering_riktig_ved_feil() {
+
+    }
+
+    @Test
+    fun skal_motta_kvittering_når_den_er_lagt_på_kø() {
+
+    }
+}
