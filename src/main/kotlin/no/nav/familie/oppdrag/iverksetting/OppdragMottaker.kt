181468904884952ba065df6cdd8009ce80a7e2d5
Jørn Ola Birkeland <jorn.ola.birkeland@nav.no>
2020-01-08 12:19:57 +0100

    Start rigging av config for distribuert tx mellom jsm og db

--- src/main/kotlin/no/nav/familie/oppdrag/iverksetting/OppdragMottaker.kt
+++ src/main/kotlin/no/nav/familie/oppdrag/iverksetting/OppdragMottaker.kt
@@ -1,26 +1,30 @@
 package no.nav.familie.oppdrag.iverksetting
 
+import no.nav.familie.oppdrag.repository.OppdragProtokollRepository
 import no.trygdeetaten.skjema.oppdrag.Oppdrag
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.env.Environment
 import org.springframework.jms.annotation.JmsListener
 import org.springframework.stereotype.Service
 import javax.jms.TextMessage
 
 @Service
-class OppdragMottaker(val env: Environment) {
+class OppdragMottaker(
+        @Autowired val oppdragProtokollRepository: OppdragProtokollRepository,
+        val env: Environment) {
 
-    @JmsListener(destination = "\${oppdrag.mq.mottak}")
+    @JmsListener(destination = "\${oppdrag.mq.mottak}", containerFactory = "jmsListenerContainerFactory")
     fun mottaKvitteringFraOppdrag(melding: TextMessage) {
         var svarFraOppdrag = melding.text as String
         if (!env.activeProfiles.contains("dev")) {
             svarFraOppdrag = svarFraOppdrag.replace("oppdrag xmlns", "ns2:oppdrag xmlns:ns2")
         }
 
-        handterKvittering(svarFraOppdrag)
+        lesKvittering(svarFraOppdrag)
     }
 
-    fun handterKvittering(svarFraOppdrag: String): Status {
+    fun lesKvittering(svarFraOppdrag: String): Status {
         val oppdragKvittering = Jaxb().tilOppdrag(svarFraOppdrag)
         val status = hentStatus(oppdragKvittering)
         val fagsakId = hentFagsakId(oppdragKvittering)
