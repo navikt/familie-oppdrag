579e575b783e44bb5f2f195dab8c914c64838700
Martine Enger <marenger@student.matnat.uio.no>
2020-01-15 12:25:14 +0100

    Eksponer endepunkt for å sette i gang avstemming

--- src/main/kotlin/no/nav/familie/oppdrag/rest/OppdragController.kt
+++ src/main/kotlin/no/nav/familie/oppdrag/rest/OppdragController.kt
@@ -2,17 +2,16 @@ package no.nav.familie.oppdrag.rest
 
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.familie.kontrakter.felles.oppdrag.Utbetalingsoppdrag
-import no.nav.familie.kontrakter.felles.oppdrag.behandlingsIdForFørsteUtbetalingsperiode
 import no.nav.familie.oppdrag.iverksetting.OppdragMapper
-import no.nav.familie.oppdrag.iverksetting.OppdragSender
-import no.nav.familie.oppdrag.repository.OppdragProtokoll
-import no.nav.familie.oppdrag.repository.OppdragProtokollRepository
 import no.nav.familie.oppdrag.service.OppdragService
 import no.nav.security.token.support.core.api.ProtectedWithClaims
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.time.LocalDateTime
 import javax.validation.Valid
 
 
@@ -29,5 +28,17 @@ class OppdragController(@Autowired val oppdragService: OppdragService,
 
         oppdragService.opprettOppdrag(utbetalingsoppdrag,oppdrag)
         return ResponseEntity.ok().body(Ressurs.Companion.success("Oppdrag sendt ok"))
+    }
+
+    @PostMapping(path = ["/avstemming"])
+    fun sendAvstemming(@RequestParam("fom") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) fom: LocalDateTime,
+                       @RequestParam("tom") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) tom: LocalDateTime
+    ): ResponseEntity<Ressurs<String>> {
+        LOG.info("Avstemmer oppdrag for $fom til $tom")
+        return ResponseEntity.ok().body(Ressurs.Companion.success("Avstemming sendt ok"))
+    }
+
+    companion object {
+        val LOG = LoggerFactory.getLogger(OppdragController::class.java)
     }
 }
