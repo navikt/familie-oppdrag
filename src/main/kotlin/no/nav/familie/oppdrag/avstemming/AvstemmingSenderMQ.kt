425f2915a92bd6f723f53406adf9414974c6f473
Martine Enger <marenger@student.matnat.uio.no>
2020-01-28 14:01:45 +0100

    Prøver å sette targetClient på køen/destinasjonen slik at RFH header ikke settes

--- src/main/kotlin/no/nav/familie/oppdrag/avstemming/AvstemmingSenderMQ.kt
+++ src/main/kotlin/no/nav/familie/oppdrag/avstemming/AvstemmingSenderMQ.kt
@@ -20,10 +20,18 @@ class AvstemmingSenderMQ(val jmsTemplateAvstemming: JmsTemplate,
 
         val avstemmingXml = JaxbAvstemmingsdata.tilXml(avstemmingsdata)
         try {
-            jmsTemplateAvstemming.send { session ->
+/*            jmsTemplateAvstemming.send { session ->
+                session.createProducer(
+                        MQQueue(jmsTemplateAvstemming.defaultDestinationName).apply {
+                            targetClient = WMQConstants.WMQ_CLIENT_NONJMS_MQ
+                        })
                 val msg = session.createTextMessage(avstemmingXml)
                 msg
-            }
+            }*/
+            jmsTemplateAvstemming.convertAndSend(
+                    "queue:///${jmsTemplateAvstemming.defaultDestinationName}?targetClient=1",
+                    avstemmingXml
+            )
             LOG.info("Sendt Avstemming-XML på kø ${jmsTemplateAvstemming.defaultDestinationName} til OS")
         } catch (e: JmsException) {
             LOG.error("Klarte ikke sende Avstemming til OS. Feil: ", e)
