6bdd133fea1408352d76ff9bc7fc4e61e20362e6
Martine Enger <marenger@student.matnat.uio.no>
2020-11-17 17:18:57 +0100

    Sett riktig avleverendeTransaksjonNokkel i grensesnittavstemming for EF-ytelser

--- src/main/kotlin/no/nav/familie/oppdrag/grensesnittavstemming/GrensesnittavstemmingMapper.kt
+++ src/main/kotlin/no/nav/familie/oppdrag/grensesnittavstemming/GrensesnittavstemmingMapper.kt
@@ -83,7 +83,7 @@ class GrensesnittavstemmingMapper(private val oppdragsliste: List<OppdragLager>,
                 Detaljdata().apply {
                     this.detaljType = detaljType
                     this.offnr = utbetalingsoppdrag.aktoer
-                    this.avleverendeTransaksjonNokkel = fagområde
+                    this.avleverendeTransaksjonNokkel = fagområdeTilAvleverendeKomponentKode(fagområde)
                     this.tidspunkt = oppdrag.avstemmingTidspunkt.format(tidspunktFormatter)
                     if (detaljType in listOf(DetaljType.AVVI, DetaljType.VARS) && oppdrag.kvitteringsmelding != null) {
                         val kvitteringsmelding = fraKvitteringTilKvitteringsmelding(oppdrag.kvitteringsmelding)
