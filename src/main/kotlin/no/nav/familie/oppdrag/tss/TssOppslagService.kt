b344285f4ff22dec23be593a07eaf78163585831
Stig Helge Strøm <stig.helge.strom@nav.no>
2022-06-28 16:04:35 +0200

    Hardkoding

--- src/main/kotlin/no/nav/familie/oppdrag/tss/TssOppslagService.kt
+++ src/main/kotlin/no/nav/familie/oppdrag/tss/TssOppslagService.kt
@@ -3,11 +3,16 @@ package no.nav.familie.oppdrag.tss
 import no.nav.familie.oppdrag.iverksetting.Jaxb
 import no.rtv.namespacetss.ObjectFactory
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.core.io.Resource
 import org.springframework.stereotype.Service
 
 @Service
 class TssOppslagService(private val tssMQClient: TssMQClient) {
     private val secureLogger = LoggerFactory.getLogger("secureLogger")
+
+    @Value("classpath:tss/910.xml")
+    lateinit var b910: Resource
 
     fun hentInformasjonOmSamhandler(orgNr: String): String {
         val objectFactory = ObjectFactory()
@@ -21,6 +26,7 @@ class TssOppslagService(private val tssMQClient: TssMQClient) {
         val tidOFF1 = objectFactory.createTidOFF1()
         tidOFF1.idOff = orgNr
         tidOFF1.kodeIdType = "ORG"
+        tidOFF1.kodeSamhType = null
         samhandlerIDataB910.ofFid = tidOFF1
 
         servicerutiner.samhandlerIDataB910 = samhandlerIDataB910
@@ -30,8 +36,11 @@ class TssOppslagService(private val tssMQClient: TssMQClient) {
         val tssSamhandlerData = objectFactory.createTssSamhandlerData()
         tssSamhandlerData.tssInputData = tssInputData
         val xml = Jaxb.tilXml(tssSamhandlerData)
+
+        val xml2 = lesFil("/tss/910.xml").replace("BYTTUT", orgNr)
+
         secureLogger.info("Sender melding til tss: $xml")
-        val response = tssMQClient.kallTss(xml)
+        val response = tssMQClient.kallTss(xml2)
         secureLogger.info("B910 = Response=$response")
         return response
     }
@@ -54,5 +63,12 @@ class TssOppslagService(private val tssMQClient: TssMQClient) {
         val response = tssMQClient.kallTss(Jaxb.tilXml(tssSamhandlerData))
         secureLogger.info("B940 = Response=$response")
         return response
+    }
+
+    private fun lesFil(fileName: String): String {
+        val res = b910
+
+        val file = res.file
+        return file.readText(Charsets.UTF_8)
     }
 }
