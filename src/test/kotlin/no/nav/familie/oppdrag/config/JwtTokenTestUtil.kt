8ae6f7d22b8f99a2d3a8068ee66aef8de08e1a1b
FredrikP <57641333+fredrikpf@users.noreply.github.com>
2026-05-22 16:29:09 +0200

    Legg til det som mangler for at tester skal kjøre

---
+++ src/test/kotlin/no/nav/familie/oppdrag/config/JwtTokenTestUtil.kt
@@ -0,0 +1,58 @@
+package no.nav.familie.oppdrag.config
+
+import no.nav.security.mock.oauth2.MockOAuth2Server
+
+object JwtTokenTestUtil {
+    fun lagAzureAdToken(
+        mockOAuth2Server: MockOAuth2Server,
+        subject: String = "test-user@nav.no",
+        audience: String = "aud-localhost",
+        groups: List<String> = emptyList(),
+        tilleggsClaims: Map<String, Any> = emptyMap(),
+    ): String =
+        mockOAuth2Server
+            .issueToken(
+                issuerId = "azuread",
+                subject = subject,
+                audience = audience,
+                claims = mapOf("groups" to groups, "preferred_username" to subject) + tilleggsClaims,
+            ).serialize()
+
+    fun lagUtgaattToken(
+        mockOAuth2Server: MockOAuth2Server,
+        subject: String = "test-user@nav.no",
+        audience: String = "aud-localhost",
+    ): String {
+        val expiry = System.currentTimeMillis() / 1000 - 3600 // 1 time siden
+        return mockOAuth2Server
+            .issueToken(
+                issuerId = "azuread",
+                subject = subject,
+                audience = audience,
+                expiry = expiry,
+            ).serialize()
+    }
+
+    fun lagTokenMedFeilIssuer(
+        mockOAuth2Server: MockOAuth2Server,
+        subject: String = "test-user@nav.no",
+        audience: String = "aud-localhost",
+    ): String =
+        mockOAuth2Server
+            .issueToken(
+                issuerId = "wrong-issuer",
+                subject = subject,
+                audience = audience,
+            ).serialize()
+
+    fun lagTokenMedFeilAudience(
+        mockOAuth2Server: MockOAuth2Server,
+        subject: String = "test-user@nav.no",
+    ): String =
+        mockOAuth2Server
+            .issueToken(
+                issuerId = "azuread",
+                subject = subject,
+                audience = "wrong-audience",
+            ).serialize()
+}
